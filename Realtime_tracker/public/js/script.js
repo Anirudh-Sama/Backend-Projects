78d241c51a0776137b481c5a0e69ce47b5e9c448
Anirudh-Sama <anirudhsingh.as61@gmail.com>
2024-08-26 19:27:34 +0530

    Finally completed real-time tracker

--- Realtime_tracker/public/js/script.js
+++ Realtime_tracker/public/js/script.js
@@ -13,3 +13,27 @@ if(navigator.geolocation){
         timeout: 5000,
     })
 }
+
+const map = L.map("map").setView([0, 0], 16);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "Anirudh Singh"
+}).addTo(map);
+
+const markers = {};
+
+socket.on("receive-location", (data) => {
+    const {id, latitude, longitude} = data;
+    map.setView([latitude, longitude]);
+    if(markers[id]){
+        markers[id].setLatLng([latitude, longitude]);
+    }else{
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+})
+
+socket.on("user-disconnected", () => {
+    if(markers[id]){
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+})

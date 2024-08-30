cdd82e7f1f1958a9d567093f95b3351a794c224a
Anirudh-Sama <anirudhsingh.as61@gmail.com>
2024-08-30 20:56:09 +0530

    Create a frontend file for the project

--- Chess.com_clone/app.js
+++ Chess.com_clone/app.js
@@ -1,4 +1,25 @@
 const express = require("express");
+const path = require("path");
 const http = require("http");
 const socket = require("socket.io")
+const {Chess} = require("chess.js")
+
 const app = express();
+const server = http.createServer(app);
+
+const io = socket(server);
+const chess = new Chess();
+
+let players = {};
+let currentPlayer = "W";
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+server.listen(3000, function() {
+    console.log("Listening on port 3000");
+});

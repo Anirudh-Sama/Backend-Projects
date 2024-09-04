fdda9a334bdb5c6e1eeaa9b7b7a00d2925a8d2fc
Anirudh-Sama <anirudhsingh.as61@gmail.com>
2024-09-04 08:45:57 +0530

    Created a chessboard on the frontend and worked on the functionalities of the chess pieces

--- Chess.com_clone/public/js/chessgame.js
+++ Chess.com_clone/public/js/chessgame.js
@@ -6,3 +6,44 @@ const boardElement = document.querySelector('.chessboard');
 // socket.on("connection received", function(){
 //     console.log("This is also received");
 // })
+
+let draggedPiece = null;
+let sourceSquare = null;
+let playerRole = null;
+
+const renderBoard = () => {
+    const board = chess.board();
+    boardElement.innerHTML = "";
+    board.forEach((row, rowIndex) => {
+        row.forEach((square, squareIndex) => {
+            const squareElement = document.createElement("div");
+            squareElement.classList.add(square, 
+                (rowIndex + squareIndex) % 2 === 0 ? "light" : "dark"
+            );
+            squareElement.dataset.row = rowIndex;
+            squareElement.dataset.col = squareIndex;
+
+            if(square){
+                const pieceElement = document.createElement("div");
+                pieceElement.classList.add("piece", square.color === "w" ? "white" : "black");
+                pieceElement.innerText = "";
+                pieceElement.draggable = playerRole === square.color;
+                pieceElement.addEventListener("dragStart", (e) => {
+                    if(pieceElement.draggable0){
+                        draggedPiece = pieceElement;
+                        sourceSquare = {row: rowIndex, col: squareIndex};
+                        e.dataTransfer.setData("text/plain", "");
+                    }
+                });
+                pieceElement.addEventListener("dragend", (e) => {
+                    draggedPiece = null;
+                    sourceSquare = null;
+                })
+            }
+        });
+    });
+};
+
+const handleMove = () => {};
+
+const getpieceUnicode = () => {};
